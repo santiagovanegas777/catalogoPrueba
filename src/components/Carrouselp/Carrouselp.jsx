b156633092e8424fcd611f143e655476fb5dd5f0
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2024-03-10 13:56:06 +0100

    responsive3

--- src/components/Carrouselp/Carrouselp.jsx
+++ src/components/Carrouselp/Carrouselp.jsx
@@ -1,20 +1,31 @@
 const Carrouselp = () => {
     return (
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1707666216/estilo/ww8izuvdmw3j0sqlturf.jpg"  class="d-block w-100" alt="estiloImagen"/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1707666627/estilo/ub0alefhxxr3qhvxbkmg.jpg" class="d-block w-100" alt="gatos"/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1710067799/estilo/uwtasxilwchi8kwv52jh.jpg" class="d-block w-100" alt="prueba"/>
-    </div>
-  </div>
-</div>
+        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1710067799/estilo/uwtasxilwchi8kwv52jh.jpg" class="d-block w-100" alt="bob esponja"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1710075288/estilo/otgbcdl2pgvsejlo5huk.jpg" class="d-block w-100" alt="gatos"/>
+          </div>
+          <div class="carousel-item">
+            <img src="https://res.cloudinary.com/dz3qrv6vz/image/upload/v1707666216/estilo/ww8izuvdmw3j0sqlturf.jpg" class="d-block w-100" alt="prueba"/>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     )
 }
 
 export default Carrouselp
+
+
+
 
